56ff86aa639bf291081c1b3f67dd80bf092be49a
Roberto Kedmenec <rexerd96@gmail.com>
2018-06-21 07:40:45 +0200

    Add basic structure.

--- src/App.js
+++ src/App.js
@@ -2,85 +2,87 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import Route from 'react-router-dom/Route';
-
+import Sidebar from './Sidebar/Sidebar.js'
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <ul>
-          <li><a href="login">Login</a></li>
-          <li><a href="register">Register</a></li>
-          <li><a href="character">character</a></li>
-          <li><a href="leaderboard">Login</a></li>
-          <li><a href="battlehistory">battlehistory</a></li>
-          <li><a href="battle">battle</a></li>
-          </ul>
-          <Route
-            path="/" exact render={
-              () => {
-                return (
-                  <h1>Welcome home</h1>
-                );
+          <div className="row">
+            <div className="col-lg-3 ">
+            <Sidebar />
+            </div>
+            <div className="col-lg-9">
+
+            <Route
+              path="/" exact render={
+                () => {
+                  return (
+                    <h1>Welcome home</h1>
+                  );
+                }
               }
-            }
-          />
-          <Route
-            path="/login" render={
-              () => {
-                return (
-                  <h1>Welcome about</h1>
-                );
+            />
+            <Route
+              path="/login" render={
+                () => {
+                  return (
+                    <h1>Welcome about</h1>
+                  );
+                }
               }
-            }
-          />
-          <Route
-            path="/register" exact render={
-              () => {
-                return (
-                  <h1>Welcome home</h1>
-                );
+            />
+            <Route
+              path="/register" exact render={
+                () => {
+                  return (
+                    <h1>Welcome home</h1>
+                  );
+                }
               }
-            }
-          />
-          <Route
-            path="/character" render={
-              () => {
-                return (
-                  <h1>Welcome about</h1>
-                );
+            />
+            <Route
+              path="/character" render={
+                () => {
+                  return (
+                    <h1>Welcome about</h1>
+                  );
+                }
               }
-            }
-          />
-          <Route
-            path="/leaderboard" render={
-              () => {
-                return (
-                  <h1>Welcome about</h1>
-                );
+            />
+            <Route
+              path="/leaderboard" render={
+                () => {
+                  return (
+                    <h1>Welcome about</h1>
+                  );
+                }
               }
-            }
-          />
-          <Route
-            path="/battlehistory" render={
-              () => {
-                return (
-                  <h1>Welcome about</h1>
-                );
+            />
+            <Route
+              path="/battlehistory" render={
+                () => {
+                  return (
+                    <h1>Welcome about</h1>
+                  );
+                }
               }
-            }
-          />
-          <Route
-            path="/battle" render={
-              () => {
-                return (
-                  <h1>Welcome about</h1>
-                );
+            />
+            <Route
+              path="/battle" render={
+                () => {
+                  return (
+                    <h1>Welcome about</h1>
+                  );
+                }
               }
-            }
-          />
+            />
+          </div>
         </div>
+        </div>
+      
       </Router>
+      
     );
   }
 }
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <div>
+            <ul>
+                <li className="classItem"><a href="login">Login</a></li>
+                <li className="classItem"><a href="register">Register</a></li>
+                <li className="classItem"><a href="character">character</a></li>
+                <li className="classItem"><a href="leaderboard">Login</a></li>
+                <li className="classItem"><a href="battlehistory">battlehistory</a></li>
+                <li className="classItem"><a href="battle">battle</a></li>
+            </ul>
+        </div>
+    );
+}
+export default Sidebar;
